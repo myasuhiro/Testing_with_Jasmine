ee8f77ca1ffd03c2a552d1a4f68913ab5ef9847e
yasuhiro.m <yasuhiro@m.com>
2023-11-28 19:26:55 -0800

    save on the way

---
+++ tip-pool/payments.test.js
@@ -0,0 +1,28 @@
+describe('Payments test (with setup and tear-down)', function() {
+  beforeEach(function() {
+    billAmtInput.value = 100;
+    tipAmtInput.value = 20;
+  });
+
+  it('should add a new payment to allPayments on submitPaymentInfo()', function() {
+    submitPaymentInfo();
+
+    expect(Object.keys(allPayments).length).toEqual(1);
+    expect(allPayments['payment1'].billAmt).toEqual('100');
+    expect(allPayments['payment1'].tipAmt).toEqual('20');
+    expect(allPayments['payment1'].tipPercent).toEqual(20);
+  });
+
+  afterEach(function() {
+    billAmtInput.value = '';
+    tipAmtInput.value = '';
+    paymentTbody.innerHTML = '';
+    summaryTds[0].innerHTML ='';
+    summaryTds[1].innerHTML ='';
+    summaryTds[2].innerHTML ='';
+    serverTbody.innerHTML = '';
+    paymentId = 0;
+    allPayments = {};
+  })
+  
+});
